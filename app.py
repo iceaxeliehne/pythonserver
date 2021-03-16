1ed8d6d2645edafb3171713223a91f1404428411
iceaxeliehne <iceaxe.liehne@gmail.com>
2021-03-16 04:30:09 +0000

    new changes

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+#socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="https://5000-crimson-canid-71sm3425.ws-us03.gitpod.io/")
+
+@app.route('/')
+def sessions():
+    return render_template('index.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+@socketio.on('image')
+def image(data_image):
+    print("hello")
+    sbuf = StringIO()
+    sbuf.write(data_image)
+
+    # decode and convert into image
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    ## converting RGB to BGR, as opencv standards
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+    # Process the image frame
+    frame = imutils.resize(frame, width=700)
+    frame = cv2.flip(frame, 1)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpg;base64,'
+    stringData = b64_src + stringData
+
+    # emit the frame back
+    emit('response_back', stringData)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
+
+
+
